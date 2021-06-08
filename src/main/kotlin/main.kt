dd3d79b6a182ac37d84a773f2df35f720e6c4744
DiegoFrancoPortela <dfrancoportela@danielcastelao.org>
2021-06-08 09:49:05 +0200

    Ejercicio2 Merge (Squash) con Rama Main.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,34 @@
+import kotlin.random.Random
+
 fun main(args: Array<String>) {
 
-    //Ejercicio 1.
-
+    // ----------------------------- EJERCICIO 1 ----------------------------------
+    println("EJERCICIO 1")
+    println("")
     var i = 0
     for (x in Ejercicio1(11).tabla7){
         println("7 x $i = $x")
         i++
     }
+
+    println("-----------")
+    println()
+    println("EJERCICIO 2")
+    println("")
+    // ----------------------------------------------------------------------------
+    // ----------------------------- EJERCICIO 2 ----------------------------------
+
+    var x = 1
+    val tamañoArray = 10
+
+    val claseRandom = Ejercicio2(tamañoArray){
+        Random.nextInt(50,250)
+    }
+
+    claseRandom.nRandom.forEach {
+        println("- $it")
+        x++
+    }
+
+    println("-----------")
 }
