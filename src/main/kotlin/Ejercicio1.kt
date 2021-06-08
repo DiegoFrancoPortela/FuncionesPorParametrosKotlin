69f4e725726f9e597ac6e12216ad898041bc7a47
DiegoFrancoPortela <dfrancoportela@danielcastelao.org>
2021-06-08 09:30:54 +0200

    Ejercicio1.kt terminado.

---
+++ src/main/kotlin/Ejercicio1.kt
@@ -0,0 +1,5 @@
+class Ejercicio1 (size: Int) {
+
+    var tabla7 = Array(size){it * 7}
+
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,10 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+
+    //Ejercicio 1
+
+    var i = 0
+    for (x in Ejercicio1(11).tabla7){
+        println("7 x $i = $x")
+        i++
+    }
 }
