63473167103b9eb134a3db5a47524310b8bb8caa
DiegoFrancoPortela <dfrancoportela@danielcastelao.org>
2021-06-08 09:48:08 +0200

    Ejercicio2 terminado.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,10 @@
+import kotlin.random.Random
+
 fun main(args: Array<String>) {
 
     // ----------------------------- EJERCICIO 1 ----------------------------------
-
+    println("EJERCICIO 1")
+    println("")
     var i = 0
     for (x in Ejercicio1(11).tabla7){
         println("7 x $i = $x")
@@ -9,10 +12,23 @@ fun main(args: Array<String>) {
     }
 
     println("-----------")
+    println()
+    println("EJERCICIO 2")
+    println("")
     // ----------------------------------------------------------------------------
     // ----------------------------- EJERCICIO 2 ----------------------------------
 
+    var x = 1
+    val tamañoArray = 10
 
+    val claseRandom = Ejercicio2(tamañoArray){
+        Random.nextInt(50,250)
+    }
+
+    claseRandom.nRandom.forEach {
+        println("- $it")
+        x++
+    }
 
     println("-----------")
 }
